9d1903d218facf61769e616e18608c0fb8235ced
armankhan <armaancomps@gmail.com>
2022-12-29 20:24:05 +0530

    Scafffold created
    1.Company
    2.Products
    3.Customers

---
+++ app/Controllers/CustomersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class CustomersController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
